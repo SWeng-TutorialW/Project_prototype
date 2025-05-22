d7ec1945cea792b4288664eab386a861943fcf14
nissim <nissimderi123@gmail.com>
2025-05-22 11:32:50 +0300

    added flower class for database

---
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/Flower.java
@@ -0,0 +1,40 @@
+package il.cshaifasweng.OCSFMediatorExample.server;
+
+public class Flower {
+    private String flowerName;
+    private double flowerPrice;
+    private String flowerType;
+
+    public Flower(){}
+
+    public Flower(String flowerName, double flowerPrice, String flowerType){
+        super();
+        this.flowerName = flowerName;
+        this.flowerPrice = flowerPrice;
+        this.flowerType = flowerType;
+    }
+
+    public double getFlowerPrice() {
+        return flowerPrice;
+    }
+
+    public String getFlowerName() {
+        return flowerName;
+    }
+
+    public String getFlowerType() {
+        return flowerType;
+    }
+
+    public void setFlowerName(String flowerName) {
+        this.flowerName = flowerName;
+    }
+
+    public void setFlowerPrice(double flowerPrice) {
+        this.flowerPrice = flowerPrice;
+    }
+
+    public void setFlowerType(String flowerType) {
+        this.flowerType = flowerType;
+    }
+}
