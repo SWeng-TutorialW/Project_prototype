d9128344b69386e3c076606958a4c0494555db3a
asafy <asafyaakov555@gmail.com>
2025-05-23 11:50:12 +0300

    aaa

---
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
@@ -0,0 +1,106 @@
+package il.cshaifasweng.OCSFMediatorExample.server;
+
+
+import il.cshaifasweng.OCSFMediatorExample.entities.CatalogUpdateEvent;
+import il.cshaifasweng.OCSFMediatorExample.entities.Flower;
+import il.cshaifasweng.OCSFMediatorExample.server.ocsf.AbstractServer;
+import il.cshaifasweng.OCSFMediatorExample.server.ocsf.ConnectionToClient;
+import java.io.IOException;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
+import il.cshaifasweng.OCSFMediatorExample.entities.Flower;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import il.cshaifasweng.OCSFMediatorExample.entities.Warning;
+import il.cshaifasweng.OCSFMediatorExample.server.ocsf.SubscribedClient;
+import org.hibernate.Session;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+
+public class SimpleServer extends AbstractServer {
+	private static ArrayList<SubscribedClient> SubscribersList = new ArrayList<>();
+
+	public SimpleServer(int port) {
+		super(port);
+
+	}
+
+	@Override
+	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+		String msgString = msg.toString();
+		if (msgString.startsWith("#warning")) {
+			Warning warning = new Warning("Warning from server!");
+			try {
+				client.sendToClient(warning);
+				System.out.format("Sent warning to client %s\n", client.getInetAddress().getHostAddress());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else if(msgString.startsWith("add client")){
+			SubscribedClient connection = new SubscribedClient(client);
+			SubscribersList.add(connection);
+			try {
+				client.sendToClient("client added successfully");
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		else if(msgString.startsWith("remove client")){
+			if(!SubscribersList.isEmpty()){
+				for(SubscribedClient subscribedClient: SubscribersList){
+					if(subscribedClient.getClient().equals(client)){
+						SubscribersList.remove(subscribedClient);
+						break;
+					}
+				}
+			}
+		}
+		else if(msgString.startsWith("getCatalogTable"))
+		{
+			try {
+				Session session = App.getSessionFactory().openSession();
+				session.beginTransaction();
+
+				CriteriaBuilder builder = session.getCriteriaBuilder();
+				CriteriaQuery<Flower> query = builder.createQuery(Flower.class);
+				query.from(Flower.class);
+
+				List<Flower> flowerList = session.createQuery(query).getResultList();
+				session.getTransaction().commit();
+				session.close();
+
+				CatalogUpdateEvent event = new CatalogUpdateEvent(flowerList);
+				client.sendToClient(event);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	public void sendToAllClients(String message) {
+		try {
+			for (SubscribedClient subscribedClient : SubscribersList) {
+				subscribedClient.getClient().sendToClient(message);
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
+}
+
+
+
+
